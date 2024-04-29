a4ff8688faa1325b38dae2d3c53b8c5592bd978b
LongPham <135238591+RamennSama@users.noreply.github.com>
2024-04-29 10:00:18 +0700

    Update List01.kt

--- List01.kt
+++ List01.kt
@@ -1,12 +1,11 @@
 
 fun main(){
     val nums : List<Int> = listOf(1, 2, 3, 10, 4, 6, 5, 9, 8, 7) // ko cần List<Int> cũng được
-
     val findNum = nums.contains(3) // Kiểm tra xem List có chứa một phần tử cụ thể không
     val sum = nums.sum() // tính tổng
     val numSort = nums.sorted() // sắp xếp list
 //  val name : MutableList<String> = mutableListOf("Ha", "Long", "Giau", "Qui", "Son", "Khoa") // ko cần MutableLis<String> cũng được
-    val name = mutableListOf("Ha", "Long", "Giau", "Qui", "Son", "Khoa")
+    val name = mutableListOf<String> ("Ha", "Long", "Giau", "Qui", "Son", "Khoa")
     name.add("Thang")
     name.remove("Son")
     // for(x in nums){
@@ -26,14 +25,19 @@ fun main(){
     println(numSort) // giong toString trong java
     println(10 in nums)
     println("Thang" in name)
+    name.forEach { print("$it ")}
+    nums.forEach { print("$it ")}
+
     val resultList : List<Int> = nums
     println(resultList[0]);
 }
 
+// - List: bạn không thể chỉnh sửa loại danh sách này sau khi tạo.
+// - MutableList: bạn có thể chỉnh sửa được loại danh sách này sau khi tạo, nghĩa là bạn có thể thêm, 
+// xoá hoặc cập nhật các phần tử của danh sách đó.
+
 // val nums : List<Int> = listOf(1, 2, 3, 10, 4, 6, 5, 9, 8, 7) // ko cần List<Int> cũng được
 // val nums = listOf(1, 2, 3, 10, 4, 6, 5, 9, 8, 7) // ko cần List<Int> cũng được
-// Trong Kotlin, List được coi là một cấu trúc dữ liệu không thay đổi (immutable), điều này có nghĩa là bạn không thể thay đổi nó sau 
-// khi đã khởi tạo. Để thêm phần tử vào một List, bạn cần sử dụng một cấu trúc dữ liệu có thể thay đổi như MutableList.
 // val name : MutableList<String> = mutableListOf("Ha", "Long", "Giau", "Qui", "Son", "Khoa") // ko cần MutableLis<String> cũng được
 // val name = mutableListOf("Ha", "Long", "Giau", "Qui", "Son", "Khoa")
 
